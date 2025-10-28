bdb1f34b8177c21b4c49812ace60aac69c246ae9
shjung-dev <shglf2000@gmail.com>
2025-10-28 16:36:02 +0800

    Initial Commit

---
+++ Instagram LoginPage Clone/Instagram_LoginPage_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_LoginPage_CloneApp.swift
+//  Instagram LoginPage Clone
+//
+//  Created by SangHil on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_LoginPage_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

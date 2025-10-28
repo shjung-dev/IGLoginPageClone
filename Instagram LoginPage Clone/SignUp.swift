024f9002b5b513cfa539fed54b80becb585ac162
shjung-dev <shglf2000@gmail.com>
2025-10-28 23:57:28 +0800

    [Feature] Completed Login Page. Left off at the part where I linked Login Page to Sign Up page UI and can immediately start working on the Sign Up Page UI

---
+++ Instagram LoginPage Clone/SignUp.swift
@@ -0,0 +1,19 @@
+//
+//  SignUp.swift
+//  Instagram LoginPage Clone
+//
+//  Created by SangHil on 28/10/25.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text("This will be a Sign Up Page UI")
+            .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    SignUp()
+}
